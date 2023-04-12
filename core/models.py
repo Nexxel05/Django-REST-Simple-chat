9cb85f56ac67f5ab1c95cbfad54a137864745d72
Kostyantyn Chernyk <chernykko@gmail.com>
2023-04-12 14:23:04 +0100

    Register app in project settings, create User, Thread, Message models and migrate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Thread(models.Model):
+    participants = models.ManyToManyField(User, related_name="threads")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User,
+        related_name="messages",
+        on_delete=models.CASCADE
+    )
