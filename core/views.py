5d99a147b229463423b92774437c71082946aabd
Kostyantyn Chernyk <chernykko@gmail.com>
2023-04-13 13:46:00 +0100

    Create views for thread and message serializers

--- core/views.py
+++ core/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from rest_framework import viewsets, mixins, generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 
-# Create your views here.
+from core.models import Thread, Message
+from core.serializers import (
+    ThreadSerializer,
+    MessageSerializer,
+    MessageCreateSerializer,
+    MessageChangeStatusSerializer,
+    ThreadCreateSerializer
+)
+
+
+class ThreadCreateView(
+    generics.CreateAPIView
+):
+    serializer_class = ThreadCreateSerializer
+
+
+class ThreadDeleteView(generics.RetrieveDestroyAPIView):
+    model = Thread
+    serializer_class = ThreadSerializer
+
+    def get_object(self):
+        return Thread.objects.get(id=self.kwargs["pk"])
+
+
+class UserThreadListView(generics.ListAPIView):
+    serializer_class = ThreadSerializer
+
+    def get_queryset(self):
+        return Thread.objects.filter(participants__username=self.kwargs["username"])
+
+
+@api_view(["GET"])
+def user_unread_messages_count(request, username):
+    unread_messages_count = Message.objects.filter(
+        sender__username=username,
+        is_read=False
+    ).count()
+    return Response(f"User '{username}' has {unread_messages_count} unread messages")
+
+
+class ThreadMessageCreateView(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin
+):
+    model = Message
+    serializer_class = MessageSerializer
+
+    def get_serializer_class(self) -> Serializer:
+        serializer_dict = {
+            "list": MessageSerializer,
+            "create": MessageCreateSerializer
+        }
+        return serializer_dict.get(self.action, MessageSerializer)
+
+    def get_queryset(self) -> Message:
+        return Message.objects.filter(thread__id=self.kwargs["pk"])
+
+    def create(self, request, *args, **kwargs):
+        super(ThreadMessageCreateView, self).create(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse("core:message-create", args=[self.kwargs["pk"]]))
+
+    def perform_create(self, serializer: Serializer) -> None:
+        thread = Thread.objects.get(id=self.kwargs["pk"])
+        serializer.save(
+            sender=self.request.user,
+            thread=thread
+        )
+
+
+class MessageDetailChangeStatusView(
+    viewsets.GenericViewSet,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin
+):
+    serializer_class = MessageSerializer
+    queryset = Message.objects.all()
+
+    def get_serializer_class(self):
+        serializer_dict = {
+            "retrieve": MessageSerializer,
+            "update": MessageChangeStatusSerializer
+        }
+        return serializer_dict.get(self.action)
+
+    def update(self, request, *args, **kwargs):
+        super(MessageDetailChangeStatusView, self).update(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse("core:message-detail", args=[self.kwargs["pk"]]))
